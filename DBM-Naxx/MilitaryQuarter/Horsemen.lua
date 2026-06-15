91eb82be00f774fa0092a989bd52617dc1514f3e
MarcelineVQ <marcythevq@gmail.com>
2026-06-14 23:46:35 -0700

    Four Horsemen: combine marks into 2 bars + correct mark timing
    Merge Thane/Baron (12s) and Blaumeux/Zeliek (16s) into one bar each; initial seed +positioning offset (Thane/Baron 33s, Blaumeux/Zeliek 31s) since the server 24s timer only starts once each horseman reaches its corner. Death handling tracks both partners per pair.

--- DBM-Naxx/MilitaryQuarter/Horsemen.lua
+++ DBM-Naxx/MilitaryQuarter/Horsemen.lua
@@ -27,10 +27,9 @@ local specWarnMarkOnPlayer		= mod:NewSpecialWarning("SpecialWarningMarkOnPlayer"
 local specWarnVoidZone			= mod:NewSpecialWarningYou(28863, nil, nil, nil, 1, 2)
 local yellVoidZone				= mod:NewYell(28863)
 
-local timerLadyMark				= mod:NewNextTimer(15, 28833, nil, nil, nil, 3)
-local timerZeliekMark			= mod:NewNextTimer(15, 28835, nil, nil, nil, 3)
-local timerBaronMark			= mod:NewNextTimer(12, 28834, nil, nil, nil, 3)
-local timerThaneMark			= mod:NewNextTimer(12, 28832, nil, nil, nil, 3)
+-- Marks combined into 2 bars: Korth'azz+Rivendare share the 12s cadence, Blaumeux+Zeliek the 15s
+local timerMarkMelee			= mod:NewNextTimer(12, 28832, "Mark: Thane/Baron", nil, nil, 3)
+local timerMarkCaster			= mod:NewNextTimer(16, 28833, "Mark: Blaumeux/Zeliek", nil, nil, 3)
 local timerMeteorCD				= mod:NewCDTimer(15, 57467, nil, false, nil, 3, nil, nil, true) -- default OFF
 --local timerVoidZoneCD			= mod:NewCDTimer(12.9, 28863, nil, nil, nil, 3)-- 12.9-16
 local timerHolyWrathCD			= mod:NewCDTimer(15, 28883, nil, false, nil, 3) -- default OFF
@@ -45,15 +44,13 @@ mod:SetBossHealthInfo(
 	16063, L.Zeliek		-- Zeliek
 )
 
-mod.vb.markCount = 0
-
 function mod:OnCombatStart()
-	self.vb.markCount = 0
-	timerLadyMark:Start(24)
-	timerZeliekMark:Start(24)
-	timerBaronMark:Start(24)
-	timerThaneMark:Start(24)
-	warnMarkSoon:Schedule(12)
+	self.vb.meleeAlive = 2
+	self.vb.casterAlive = 2
+	-- Server starts the 24s mark timer only once each horseman reaches its corner; add run-to-position offset
+	timerMarkCaster:Start(31)	-- Blaumeux/Zeliek: 24 + ~7s positioning
+	timerMarkMelee:Start(33)	-- Korth'azz/Rivendare: 24 + ~9s positioning
+	warnMarkSoon:Schedule(19)
 	timerMeteorCD:Start("v10-15")
 	timerHolyWrathCD:Start(15)
 	if self.Options.RangeFrame then
@@ -75,19 +72,12 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if args:IsSpellID(28832, 28833, 28834, 28835) and self:AntiSpam(5, spellId) then
-		self.vb.markCount = self.vb.markCount + 1
-		if spellId == 28833 then -- Lady Mark
-			timerLadyMark:Start(15)
-		elseif spellId == 28835 then -- Zeliek Mark
-			timerZeliekMark:Start(15)
-		elseif spellId == 28834 then -- Baron Mark
-			timerBaronMark:Start()
-		elseif spellId == 28832 then -- Thane Mark
-			timerThaneMark:Start()
-		end
+	if args:IsSpellID(28832, 28834) and self:AntiSpam(5, "markmelee") then -- Korth'azz/Rivendare share the 12s mark
+		timerMarkMelee:Start(12)
 		warnMarkSoon:Schedule(12)
+	elseif args:IsSpellID(28833, 28835) and self:AntiSpam(5, "markcaster") then -- Blaumeux/Zeliek share the ~16s mark
+		timerMarkCaster:Start(16)
+		warnMarkSoon:Schedule(16)
 	elseif args.spellId == 57463 then
 --		timerVoidZoneCD:Start()
 		if args:IsPlayer() then
@@ -128,14 +118,18 @@ end
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 16064 then
-		timerThaneMark:Cancel()
+	if cid == 16064 then -- Thane Korth'azz
 		timerMeteorCD:Cancel()
-	elseif cid == 30549 then
-		timerBaronMark:Cancel()
-	elseif cid == 16065 then
-		timerLadyMark:Cancel()
-	elseif cid == 16063 then
-		timerZeliekMark:Cancel()
+		self.vb.meleeAlive = self.vb.meleeAlive - 1
+		if self.vb.meleeAlive <= 0 then timerMarkMelee:Cancel() end
+	elseif cid == 30549 then -- Baron Rivendare
+		self.vb.meleeAlive = self.vb.meleeAlive - 1
+		if self.vb.meleeAlive <= 0 then timerMarkMelee:Cancel() end
+	elseif cid == 16065 then -- Lady Blaumeux
+		self.vb.casterAlive = self.vb.casterAlive - 1
+		if self.vb.casterAlive <= 0 then timerMarkCaster:Cancel() end
+	elseif cid == 16063 then -- Sir Zeliek
+		self.vb.casterAlive = self.vb.casterAlive - 1
+		if self.vb.casterAlive <= 0 then timerMarkCaster:Cancel() end
 	end
 end
